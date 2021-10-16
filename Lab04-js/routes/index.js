3df90ddf22378433e0dea5648bcf5a283494d882
mikalixd <cheungkukli@gmail.com>
2021-10-16 15:24:31 +0800

    git commit

--- Lab04-js/routes/index.js
+++ Lab04-js/routes/index.js
@@ -1,6 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+var loki = require('lokijs');
+
+var db = new loki('data.json', {
+    autoload: true,
+    autoloadCallback: databaseInitialize,
+    autosave: true,
+    autosaveInterval: 4000
+});
+
+// implement the autoloadback referenced in loki constructor
+function databaseInitialize() {
+    var bookings = db.getCollection("bookings");
+    if (bookings === null) {
+        bookings = db.addCollection("bookings");
+    }
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // index=filename; {} Variable
@@ -14,7 +31,9 @@ router.post('/form', function (req, res) {
       header: req.headers,
       body: req.body
   };
-
+  // Store Data in server-side
+  req.body.numTickets = parseInt(req.body.numTickets);
+  db.getCollection("bookings").insert(req.body);
   res.json(response);    
 });
 
